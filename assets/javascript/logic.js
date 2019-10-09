f8e63680966859051c8f58964cfe84eb0ea8447d
Jeff Huang <jeffshuang2@gmail.com>
2019-10-09 12:43:57 -0700

    Create firebase database and initialized it within my app

---
+++ assets/javascript/logic.js
@@ -0,0 +1,13 @@
+var config = {
+    apiKey: "AIzaSyBuFN_iZX5YoJR2WpMTVGU56g5YPW8MScs",
+    authDomain: "train-scheduler-dfc7b.firebaseapp.com",
+    databaseURL: "https://train-scheduler-dfc7b.firebaseio.com",
+    projectId: "train-scheduler-dfc7b",
+    storageBucket: "train-scheduler-dfc7b.appspot.com",
+    messagingSenderId: "982111725743",
+    appId: "1:982111725743:web:3056a3ea7a2e2fcbec4c5a"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
